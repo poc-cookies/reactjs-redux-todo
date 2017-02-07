31c29bd618fa0ab4edf69b96918a548cd2f285a3
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-02-08 02:37:25 +0300

    todos reducer function. ADD_TODO action handling. Test.

---
+++ src/todos.test.js
@@ -0,0 +1,23 @@
+import deepFreeze from 'deep-freeze';
+import {todos} from './todos';
+
+it('adds todo', () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [{
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+});
