82471394ed0d92a873f4da5135236ccdfbdd8b56
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-02-08 13:19:45 +0300

    Handling of TOGGLE_TODO action in todos reducer. Test.

--- src/todos.test.js
+++ src/todos.test.js
@@ -21,3 +21,35 @@ it('adds todo', () => {
     todos(stateBefore, action)
   ).toEqual(stateAfter);
 });
+
+it('toggles todo', () => {
+  const stateBefore = [{
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }, {
+    id: 1,
+    text: 'Go shopping',
+    completed: false
+  }];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [{
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }, {
+    id: 1,
+    text: 'Go shopping',
+    completed: true
+  }];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+});
